6488fabe007d7d8269733a5e7be1bb7be3341cf1
Ángel Álvarez <kreandostudio@gmail.com>
2019-01-03 21:06:29 +0100

    Correcion del codigo. Añadimos menu y funcionalidades de los menus. Funciona 'Bonoloto', 'Primitiva' y 'Euromillones'. Añadimos seleccion de si queremos repetir.

--- lottery_main.py
+++ lottery_main.py
@@ -11,6 +11,30 @@ def desea_salir(texto):
         return False
 
 
+def repetimos(texto):
+    if texto == "S":
+        return True
+    elif texto == "N":
+        return False
+    else:
+        return None
+
+
+def desea_repertir():
+    print
+
+    no_es_correcta = True
+    while no_es_correcta:
+        op = raw_input("¿Desea repetir ('s/S'o 'n/N'): ")
+        op = op.upper()[0]
+        if repetimos(op) == None:
+            print "\nLa opcion elegida no es correcta."
+        else:
+            no_es_correcta = False
+
+    return repetimos(op)
+
+
 def muestra_menu_inicial():
     print
     print "\n  Esto es un Juego de la Lotería que no te hará ganar pero śi te hará creertelo!! JAJAJAJA!!"
@@ -43,115 +67,120 @@ def muestra_menu_inicial():
     return op
 
 
-def bonoloto_y_primitiva(tipo):
+def primitiva_y_bonoloto(tipo):
     # Tipo: 1=Bonoloto; 2=Primitiva
     #
     # Caracteristicas comunes:
     # Seis numeros del 1 al 49 y el reintegro del 0 al 9
+    #####################################################
+
     tipo_sorteo = ["Bonoloto", "Primitiva"]
-    sorteo = []
-    reintegro = None
-    tipo = tipo - 1
+    tipo_seleccionado = tipo - 1
 
-    print
-    print "Usted ha elegido el Sorteo de la {}.\n".format(tipo_sorteo[tipo])
-    print "Esta es su combinación ganadora: "
-    for i in range(1, 8):
-        if i == 7: #Reintegro:
-            reintegro = randint(0, 9)
+    nombre = None
+    list_num = []
+    primi = []
+
+    for i in range(1, 49):
+        list_num.append(i)
+        primi.append(i)
+    list_num.sort()
+    primi.sort()
+
+    nombre = tipo_sorteo[tipo_seleccionado]
+    print "\n{}: ".format(nombre)
+
+    comprobracion = []
+    testigo = True
+    while testigo:
+        if len(primi) > 6:
+            numerete = randint(1, 49)
+            try:
+                primi.remove(numerete)
+            except ValueError:
+                comprobracion.append(numerete)
         else:
-            sorteo.append(randint(1, 49))
+            testigo = False
 
-    sorteo.sort()
+    comprobracion.sort()
 
-    x = 0
-    for element in sorteo:
-        print ".-{}".format(element)
+    print "\nNúmeros: "
+    #x = 1
+    for element in primi:
+        print ".-{}".format(element)#x, element)
+        #x += 1
 
-    print "R.: {}".format(reintegro)
-    print
-
-    print "Ahora vamos a comprobar si funciona la funcion de comprobar si hay algun elemento repetido ('es_repetido(list)'): "
-    print
-    es_repetido(sorteo)
+    print "\nReintegro: "
+    print ".-{}".format(randint(0, 9))
 
 
-def es_repetido(list):
-    numeroi = None
-    list.sort()
-    x = 0
-    for i in range(x, len(list)):
-        numeroi = list[i]
-        no_es_igual = True
-        y = x + 1
-        while no_es_igual:
-            if not y >= len(list):
-                comprobar = list[y]
-                if comprobar == numeroi:
-                    print "En en puesto {} de la lista, es igual al numero {} en el puesto {}".format(numeroi, comprobar, y)
-                    no_es_igual = False
-                else:
-                    print "El numero {} de la lista, no es igual al numero {} en el puesto {}.".format(numeroi, comprobar, y)
-                    y += 1
-            else:
-                print "Salimos del bucle"
-                no_es_igual = False
-                x += 1
+def euromillones(tipo):
+    # Tipo: 3=Euromillones
+    #
+    # Caracteristicas:
+    # .-Cinco números entre 50 distintos y 2 estrellas entre 11 distintas.
+    #######################################################################
+    tipo_sorteo = "Euromillones"
+    euromillones = []
+    estrellas = []
+    comprobar = []
+    for i in range(1, 50):
+        euromillones.append(i)
+    euromillones.sort()
+
+    for i in range(1, 11):
+        estrellas.append(i)
+    estrellas.sort()
+
+    print "\n{}: ".format(tipo_sorteo)
+    testigo = True
+    while testigo:
+        if len(euromillones) > 5:
+            numero_aleatorio = randint(1, 50)
+            try:
+                euromillones.remove(numero_aleatorio)
+            except ValueError:
+                comprobar.append(numero_aleatorio)
+        else:
+            testigo = False
+
+    testigo = True
+    while testigo:
+        if len(estrellas) > 2:
+            numero_aleatorio = randint(1, 11)
+            try:
+                estrellas.remove(numero_aleatorio)
+            except ValueError:
+                comprobar.append(numero_aleatorio)
+        else:
+            testigo = False
+
+    print "\nNúmeros: "
+    x = 1
+    for element in euromillones:
+        print "{}.-{}".format(x, element)
+        x += 1
+
+    print "\nEstrellas: "
+    x = 1
+    for element in estrellas:
+        print "{}.-{}".format(x, element)
+        x += 1
 
 
 def main():
-    no_es_correcto = True
-    quiero_salir = False
-    numero = None
-    lista_de_numeros = []
+    no_salir = True
+    while no_salir:
+        option = muestra_menu_inicial()
+        if not option == 0:
+            if option == 1 or option == 2:
+                primitiva_y_bonoloto(option)
+            elif option == 3:
+                euromillones(option)
 
-    opcion = muestra_menu_inicial()
-    if not opcion == 0:
-        if opcion == 1:
-            bonoloto_y_primitiva(opcion)
-        elif opcion == 2:
-            bonoloto_y_primitiva(opcion)
+            if not desea_repertir():
+                no_salir = False
 
-        while no_es_correcto:
-            usuario = raw_input("Introduzca la cantidad de numeros que se generaran (del 1 al 100)('q/Q' para salir): ")
-            try:
-                numero = int(usuario)
-                no_es_correcto = False
-            except ValueError:
-                if desea_salir(usuario.upper()[0]):
-                    no_es_correcto = False
-                    quiero_salir = True
-                else:
-                    print "\nError! En la lotería solo se usan números! Gracias!"
-                    raw_input("Pulse una tecla para continuar.")
-
-        if quiero_salir:
-            print "Adeu!!"
-        else:
-            print "\nSe generarán {} números aleatorios y son estos:".format(numero)
-
-            for i in range(1, numero+1):
-                if i>1:
-                    testigo = True
-                    while testigo:
-                        un_numero = randint(1, numero)
-                        if not es_repetido(un_numero, lista_de_numeros):
-                            lista_de_numeros.append(un_numero)
-                            testigo = False
-                        else:
-                            print "\nEl numero generado es repetido. Se generará otro."
-                else:
-                    lista_de_numeros.append(randint(1, numero))
-
-            lista_de_numeros.sort()
-            x = 1
-            for element in lista_de_numeros:
-                print "Elemento {}, Numero {}".format(x, element)
-                #x = x + 1
-                x += 1
-    else:
-        print "\nGracias por usar Lottery."
-        raw_input("Pulse una tecla para continuar.")
 # ----------------------------------------------------------------------------------------------------------------------
 
 
